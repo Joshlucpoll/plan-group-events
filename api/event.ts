319db7c66052e9adab5476fadf0e0d4f19e4d440
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2022-02-01 19:54:42 +0000

    Retrieve event should now work

--- api/event.ts
+++ api/event.ts
@@ -1,6 +1,28 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import admin from "firebase-admin";
+import dotenv from "dotenv";
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const { name } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+  dotenv.config();
+  const certs = JSON.parse(atob(process.env.GOOGLE_APPLICATION_CREDENTIALS));
+
+  admin.initializeApp({
+    credential: admin.credential.cert(certs),
+    databaseURL:
+      "https://plan-group-events-default-rtdb.europe-west1.firebasedatabase.app",
+  });
+
+  const db = admin.database();
+
+  const { id } = request.query;
+
+  const ref = db.ref("/events/" + id);
+  ref.once("value", function (snap) {
+    const val = snap.val();
+    if (val) {
+      response.status(200).send(val);
+    } else {
+      response.status(404);
+    }
+  });
 };
