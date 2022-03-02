5c01bdc1c14583066ac4fc7de3701a5093758b75
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2022-03-02 18:05:41 +0000

    Updated api tp use what3words id style

--- api/add.ts
+++ api/add.ts
@@ -1,12 +1,12 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import admin from "firebase-admin";
 import dotenv from "dotenv";
-import { v4 as uuidv4 } from "uuid";
+import { readFileSync } from "fs";
 
 const stringCheckBool = (str: string) =>
   str == "true" || str == "false" ? str == "true" : str;
 
-export default (request: VercelRequest, response: VercelResponse) => {
+export default async (request: VercelRequest, response: VercelResponse) => {
   dotenv.config();
   const certs = JSON.parse(
     Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS, "base64").toString(
@@ -20,7 +20,6 @@ export default (request: VercelRequest, response: VercelResponse) => {
       "https://plan-group-events-default-rtdb.europe-west1.firebasedatabase.app",
   });
 
-  const delApp = () => app.delete();
   const db = admin.database();
 
   try {
@@ -36,15 +35,29 @@ export default (request: VercelRequest, response: VercelResponse) => {
       created_at: Math.trunc(Date.now() / 1000),
       updated_at: Math.trunc(Date.now() / 1000),
     };
-    const id = uuidv4();
 
-    const ref = db.ref("/events/" + id);
-    ref.set(data).then((outcome) => {
+    // const id = uuidv4();
+    var id: string;
+    var ref;
+    while (true) {
+      const file = readFileSync("./nouns.txt", "utf-8");
+      const nouns = file.split("\n");
+      const getRanNoun = () => nouns[Math.floor(Math.random() * nouns.length)];
+      const randomNouns = [getRanNoun(), getRanNoun(), getRanNoun()];
+
+      id = randomNouns.join("-");
+
+      ref = db.ref("/events/" + id);
+      if (await ref.get().then((snap) => !snap.exists())) {
+        break;
+      }
+    }
+
+    await ref.set(data).then((outcome) => {
       response.status(201).send(id);
-      delApp();
     });
   } catch (error) {
     response.status(400).send("Could not parse params");
-    delApp();
   }
+  await app.delete();
 };
--- api/view.ts
+++ api/view.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import admin from "firebase-admin";
 import dotenv from "dotenv";
 
-export default (request: VercelRequest, response: VercelResponse) => {
+export default async (request: VercelRequest, response: VercelResponse) => {
   dotenv.config();
   const certs = JSON.parse(
     Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS, "base64").toString(
@@ -23,14 +23,10 @@ export default (request: VercelRequest, response: VercelResponse) => {
   const delApp = () => app.delete();
 
   const ref = db.ref("/events/" + id);
-  ref.once("value", function (snap) {
+  await ref.get().then((snap) => {
     const val = snap.val();
-    if (val) {
-      response.status(200).send(val);
-      delApp();
-    } else {
-      response.status(404).send("Event not found");
-      delApp();
-    }
+    if (val) response.status(200).send(val);
+    else response.status(404).send("Event not found");
   });
+  await app.delete();
 };
