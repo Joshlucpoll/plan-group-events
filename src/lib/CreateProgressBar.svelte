1cd5f6c2521b9625fc16e660412cb2d0041902c7
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2022-03-19 19:49:15 +0000

    Added to create page

---
+++ src/lib/CreateProgressBar.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  export let steps: string[];
+</script>
+
+<div class="wrapper">
+  {#each steps as step}
+    <div class="step">{step}</div>
+    {#if steps[steps.length - 1] !== step}
+      <div class="connector" />
+    {/if}
+  {/each}
+</div>
+
+<style>
+  .wrapper {
+    display: grid;
+    grid-template: none / 3rem auto 3rem auto 3rem;
+    align-items: center;
+
+    height: min-content;
+  }
+
+  .step {
+    box-sizing: border-box;
+    border: rgba(0, 0, 0, 0.5) solid 5px;
+    height: 3rem;
+    width: 3rem;
+
+    border-radius: 3rem;
+
+    position: relative;
+    text-align: center;
+  }
+
+  .connector {
+    height: 0.5rem;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+</style>
--- src/routes/create.svelte
+++ src/routes/create.svelte
@@ -1,4 +1,28 @@
-<h1>So you wanna make an event?</h1>
-<h2>Well first I'm gonna need a name:</h2>
-<input type="text" />
-<input type="date" />
+<script lang="ts">
+  import CreateProgressBar from "$lib/CreateProgressBar.svelte";
+
+  let titleValue: string;
+  let descriptionValue: string;
+  let datetime: Date;
+
+  const submitForm = () => {};
+</script>
+
+<div class="container">
+  <section>
+    <h1>So you wanna make an event?</h1>
+    <h2>Well first I'm gonna need a name:</h2>
+    <input type="text" />
+    <input type="date" />
+  </section>
+  <CreateProgressBar steps={["organiser", "title/descr", "date/location"]} />
+</div>
+
+<style>
+  section {
+    height: 100%;
+  }
+  .container {
+    display: grid;
+  }
+</style>
