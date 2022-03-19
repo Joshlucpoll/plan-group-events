ba73d47fb23c4dc7a6b7a43d46a8149daae916c8
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2022-03-19 22:33:15 +0000

    Created animations

--- src/lib/CreateProgressBar.svelte
+++ src/lib/CreateProgressBar.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
   export let steps: string[];
+
+  let currentStep = 0;
 </script>
 
 <div class="wrapper">
-  {#each steps as step}
-    <div class="step">{step}</div>
-    {#if steps[steps.length - 1] !== step}
-      <div class="connector" />
+  {#each steps as step, i}
+    <div
+      class="step"
+      style="opacity: {currentStep < i
+        ? 0.3
+        : 1}; background-color: {currentStep <= i ? 'transparent' : 'black'};"
+    >
+      <div class="step-title">
+        {step}
+      </div>
+    </div>
+    {#if i != steps.length - 1}
+      <div style="opacity: {currentStep <= i ? 0.3 : 1};" class="connector" />
     {/if}
   {/each}
 </div>
@@ -22,19 +33,26 @@
 
   .step {
     box-sizing: border-box;
-    border: rgba(0, 0, 0, 0.5) solid 5px;
+    border: black solid 5px;
     height: 3rem;
     width: 3rem;
 
     border-radius: 3rem;
 
     position: relative;
+    display: flex;
+    justify-content: center;
     text-align: center;
+  }
+
+  .step-title {
+    position: relative;
+    top: calc(100% + 1rem);
   }
 
   .connector {
     height: 0.5rem;
     width: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: black;
   }
 </style>
